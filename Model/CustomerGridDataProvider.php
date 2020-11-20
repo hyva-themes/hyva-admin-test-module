69fb1cff8db66babab4e528ebd26e82041917758
Vinai Kopp <vinai@netzarbeiter.com>
2020-11-20 12:45:36 +0100

    Add simple customer array data provider

---
+++ Model/CustomerGridDataProvider.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Hyva\AdminTest\Model;
+
+use Hyva\Admin\Api\HyvaGridArrayProviderInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Reflection\DataObjectProcessor;
+
+use function array_map as map;
+
+class CustomerGridDataProvider implements HyvaGridArrayProviderInterface
+{
+    private CustomerRepositoryInterface $customerRepository;
+
+    private SearchCriteriaBuilder $searchCriteriaBuilder;
+
+    private DataObjectProcessor $dataObjectProcessor;
+
+    public function __construct(
+        CustomerRepositoryInterface $customerRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        DataObjectProcessor $dataObjectProcessor
+    ) {
+        $this->customerRepository    = $customerRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->dataObjectProcessor   = $dataObjectProcessor;
+    }
+
+    public function getHyvaGridData(): array
+    {
+        return map(function (CustomerInterface $customer): array {
+            return $this->dataObjectProcessor->buildOutputDataArray($customer, CustomerInterface::class);
+        }, $this->customerRepository->getList($this->searchCriteriaBuilder->create())->getItems());
+    }
+}
